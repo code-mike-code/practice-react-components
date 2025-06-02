dd9dfa7fa53fc894d899334564b9633c26b390b7
Michał <m.majewski.dm@gmail.com>
2025-06-02 13:00:23 +0200

    created Weather component structure

---
+++ 05/components/Weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Weather extends React.Component {
+    state = {
+        data: null,
+    }
+
+    render() {
+        return null
+    }
+}
+
+export default Weather;
